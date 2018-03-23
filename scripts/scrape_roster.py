7747904bb599cdb39ffb772e0196bf38891f68d8
CMDR Galaxyutii <zebernst@gmail.com>
2018-03-23 10:05:04 -0400

    path modification (yet again)
    Signed-off-by: CMDR Galaxyutii <zebernst@gmail.com>

--- scripts/scrape_roster.py
+++ scripts/scrape_roster.py
@@ -2,9 +2,9 @@ if __name__ == "__main__":
     import django
     import os
     import sys
-    from distantworlds2.settings.base import SITE_ROOT
+    from pathlib import PurePath
 
-    sys.path.append(os.path.abspath(str(SITE_ROOT)))
+    sys.path.append(str(PurePath(__file__).parent))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'distantworlds2.settings.dev')
     django.setup()
 
