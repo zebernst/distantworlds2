155d7393f66b34550df5897f60eb50337a85d970
CMDR Galaxyutii <zebernst@gmail.com>
2018-03-27 14:23:43 -0400

    raise error on duplicate image upload
    Signed-off-by: CMDR Galaxyutii <zebernst@gmail.com>

--- astrophotography/forms.py
+++ astrophotography/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import *
 
@@ -7,5 +8,16 @@ class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ("image", "public", "edited", "desc")  # "waypoint", "location")
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        sha1 = hashlib.sha1()
+        for chunk in cleaned_data['image'].chunks():
+            sha1.update(chunk)
+        sha1sum = sha1.hexdigest()
+
+        if Image.objects.filter(sha1sum=sha1sum).exists():
+            raise ValidationError('Duplicate image detected. Please contact an admin for more information.')
 
 # todo: create LocationForm and add it to the ImageForm view like i did with the RegistrationForm
